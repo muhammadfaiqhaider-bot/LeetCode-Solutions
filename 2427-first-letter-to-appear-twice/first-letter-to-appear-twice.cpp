a36164f5f913971657c95f49de1153240e17881e
Muhammad Faiq Haider <muhammadfaiqhaider@gmail.com>
2026-06-18 18:22:57 +0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.04%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+
+    for (int i = 1; i < s.length(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (s[i] == s[j])
+                return s[i];
+        }
+    }
+
+    return ' ';
+}
+};
