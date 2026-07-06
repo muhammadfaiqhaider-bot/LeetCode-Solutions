9f5ed4e76b275e16a6669be8dcf5ad53c832206c
Muhammad Faiq Haider <muhammadfaiqhaider@gmail.com>
2026-07-06 07:17:39 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (53.75%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+    int freq[10] = { 0 };
+
+    while (n > 0)
+    {
+        freq[n % 10]++;
+        n /= 10;
+    }
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum = sum + (i * freq[i]);
+    }
+    return sum;
+}
+};
